59837475f0819da1e6ab604de53272c1da3fe844
julimika <119322496+julimika@users.noreply.github.com>
2023-06-13 15:01:31 -0300

    Update automacao_qa.py

--- automacao_qa.py
+++ automacao_qa.py
@@ -1,5 +1,4 @@
 from playwright.sync_api import sync_playwright
-from playwright.async_api import async_playwright
 from playwright.sync_api import Page, expect
 import time
 
@@ -14,29 +13,21 @@ with sync_playwright() as p:
     time.sleep(5)
     pagina.locator('xpath=/html/body/app-root/div/div/app-page/div/section/app-profile-select/div[1]/div/div[1]/div[1]/div[2]/div/img').click()
     time.sleep(10)
-    
-    
-    
+
+    pagina.mouse.wheel(delta_x=0 ,delta_y=6881.65) #descer até o final da página
     time.sleep(10)
+    
     expect(pagina.get_by_text("Canais Ao vivo", exact=True)).to_be_visible()
     expect(pagina.get_by_text("WATCH PARA VOCÊ", exact=True)).to_be_visible()
-    lancamentos = pagina.locator('xpath=/html/body/app-root/div/div/app-page/div/section/app-home/main/div[2]/div[3]/div/div/div/div/app-section/div/div/div[1]')
-await lancamentos.scroll_into_view_if_needed
-    time.sleep(10)
-    # expect(pagina.get_by_text("Lançamentos", exact=True)).to_be_visible()
-    # expect(pagina.get_by_text("Sugestões", exact=True)).to_be_visible()
-    # expect(pagina.get_by_text("Mais Assistidos", exact=True)).to_be_visible()
-    # expect(pagina.get_by_text("Sugestões para Você", exact=True)).to_be_visible()
+    expect(pagina.get_by_text("Lançamentos", exact=True)).to_be_visible()
+    expect(pagina.get_by_text("Continuar assistindo", exact=True)).to_be_visible()
+    expect(pagina.get_by_text("Mais Assistidos", exact=True)).to_be_visible()
+    expect(pagina.get_by_text("Sugestões para Você", exact=True)).to_be_visible()
+    expect(pagina.get_by_text("Alugue", exact=True)).to_be_visible()
 
-    
-    
-    
-
-
-    # hbo = pagina.locator('xpath=/html/body/app-root/div/div/app-page/div/section/app-home/main/div[1]/div[2]/div/div/app-default-section-description/div/div[2]/swiper/div[3]/div[1]/app-default-card/a/div').click()
-    #categorias
-    pagina.locator('xpath=/html/body/app-root/div/div/app-page/div/app-header/nav/div[1]/div/ul/li[2]/div/a/div').click()
+    pagina.locator('xpath=/html/body/app-root/div/div/app-page/div/app-header/nav/div[1]/div/ul/li[2]/div/a/div').click() #dropdown categorias
     time.sleep(5)
+    
   
 
 
