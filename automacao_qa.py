845ea48621b65a5b69ebbecfa5f6bb605c03fd2b
julimika <119322496+julimika@users.noreply.github.com>
2023-09-25 22:20:46 -0300

    Update automacao_qa.py

--- automacao_qa.py
+++ automacao_qa.py
@@ -11,23 +11,35 @@ with sync_playwright() as p:
     pagina.fill('xpath=/html/body/app-root/div/div/app-page/div/section/app-login/div/form/div[2]/app-input[2]/div/input', "123456Wa")
     pagina.get_by_role("button").click()
     time.sleep(5)
-    pagina.locator('xpath=/html/body/app-root/div/div/app-page/div/section/app-profile-select/div[1]/div/div[1]/div[1]/div[2]/div/img').click()
-    time.sleep(10)
 
-    pagina.mouse.wheel(delta_x=0 ,delta_y=6881.65) #descer até o final da página
-    time.sleep(10)
-    
-    expect(pagina.get_by_text("Canais Ao vivo", exact=True)).to_be_visible()
-    expect(pagina.get_by_text("WATCH PARA VOCÊ", exact=True)).to_be_visible()
-    expect(pagina.get_by_text("Lançamentos", exact=True)).to_be_visible()
-    expect(pagina.get_by_text("Continuar assistindo", exact=True)).to_be_visible()
-    expect(pagina.get_by_text("Mais Assistidos", exact=True)).to_be_visible()
-    expect(pagina.get_by_text("Sugestões para Você", exact=True)).to_be_visible()
-    expect(pagina.get_by_text("Alugue", exact=True)).to_be_visible()
+    pagina.locator("profile-name").click() #profile-select
+    # time.sleep(10)
 
-    pagina.locator('xpath=/html/body/app-root/div/div/app-page/div/app-header/nav/div[1]/div/ul/li[2]/div/a/div').click() #dropdown categorias
-    time.sleep(5)
+    # pagina.mouse.wheel(delta_x=0 ,delta_y=6881.65)
+    # time.sleep(10)
+    # expect(pagina.get_by_text("Canais Ao vivo", exact=True)).to_be_visible()
+    # expect(pagina.get_by_text("WATCH PARA VOCÊ", exact=True)).to_be_visible()
     
+
+    # expect(pagina.get_by_title("Lançamentos")).to_have_text("Lançamentos")
+    # expect(pagina.get_by_text("Sugestões", exact=True)).to_be_visible()
+    # expect(pagina.get_by_text("Mais Assistidos", exact=True)).to_be_visible()
+    # expect(pagina.get_by_text("Sugestões para Você", exact=True)).to_be_visible()
+
+    
+    
+    
+
+
+  
+    
+  
+
+
+
+
+
+
   
 
 
